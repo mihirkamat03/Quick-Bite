a0e5439a41f0c4af0ef0a0ccfebcd85a413fa26e
mihirkamat03 <76988536+mihirkamat03@users.noreply.github.com>
2025-08-25 01:44:06 +0530

    script.js

--- script.js
+++ script.js
@@ -13,14 +13,28 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > 3) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (i = 0; i < 3; i++) {
     slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
+  for (i = 0; i < 3; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+
+
+const btn = document.querySelector('.order-now-redirect');
+const food2 = document.querySelector('.slider-food-2');
+
+btn.addEventListener('mouseenter', () => {
+  food2.style.animation = "curve-out-2 1s ease-in-out forwards";
+  
+  // after animation end → hide it
+  food2.addEventListener('animationend', () => {
+    food2.style.display = "none";
+  }, { once: true });
+});
